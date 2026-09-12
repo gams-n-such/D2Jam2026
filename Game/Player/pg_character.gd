eb65ab0b91d7b5f5fbeb1bb142cbb3e62540e164
Denis Maximenko <manlok71@gmail.com>
2026-09-12 11:03:30 +0400

    Controls MVP

--- Game/Player/pg_character.gd
+++ Game/Player/pg_character.gd
@@ -4,18 +4,35 @@ extends RigidBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+@onready var stick_floor_ray_cast: RayCast3D = %StickFloorRayCast
+
+var _mouse_input : Vector2 = Vector2.ZERO
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		Game.open_pause_menu()
+	elif event.is_action_pressed("jump"):
+		if stick_floor_ray_cast.is_colliding():
+			var impulse := global_basis * Vector3.UP * jump_impulse
+			apply_impulse(impulse, stick_floor_ray_cast.global_position - global_position)
+			print(jump_impulse)
+	if event is InputEventMouseMotion:
+		var move := (event as InputEventMouseMotion).relative
+		#_mouse_input += move.relative
+		if move.length_squared() > 0.01:
+			var impulse := global_basis * Vector3.UP * move.x * turn_sensitivity * -1.0
+			apply_torque_impulse(impulse)
 
-@export var rotation_input_force : float = 15.0
+@export var jump_impulse : float = 10.0
+@export var rotation_input_force : float = 25.0
+@export var turn_sensitivity : float = 0.05
 
 func _physics_process(delta: float) -> void:
 	var input_dir := JamUtils.get_move_input_dir_2d()
