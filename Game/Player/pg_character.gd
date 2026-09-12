84711ad90dbc5eca785ef2df253af084dcdd3dcb
Denis Maximenko <manlok71@gmail.com>
2026-09-12 12:30:51 +0400

    Support gamepad camera in player

--- Game/Player/pg_character.gd
+++ Game/Player/pg_character.gd
@@ -9,7 +9,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var camera_input := JamUtils.get_camera_input_dir()
+	add_camera_input(camera_input * gamepad_camera_sensitivity)
 
 @onready var stick_floor_ray_cast: RayCast3D = %StickFloorRayCast
 @onready var pcam: PhantomCamera3D = %PhantomCamera3D
@@ -28,25 +29,29 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var move := (event as InputEventMouseMotion).relative
 		#_mouse_input += move.relative
-		if move.length_squared() > 0.01:
-			#var impulse := global_basis * Vector3.UP * move.x * turn_sensitivity * -1.0
-			#apply_torque_impulse(impulse)
-			var pcam_rotation_degrees: Vector3
-			pcam_rotation_degrees = pcam.get_third_person_rotation_degrees()
-			pcam_rotation_degrees.x -= move.y * turn_sensitivity
-			pcam_rotation_degrees.x = clampf(pcam_rotation_degrees.x, -80.0, 85.0)
-			pcam_rotation_degrees.y -= move.x * turn_sensitivity
-			pcam_rotation_degrees.y = wrapf(pcam_rotation_degrees.y, 0.0, 360.0)
-			pcam.set_third_person_rotation_degrees(pcam_rotation_degrees)
+		add_camera_input(move * mouse_camera_sensitivity)
+
+func add_camera_input(input : Vector2) -> void:
+	if input.length_squared() > 0.01:
+		#var impulse := global_basis * Vector3.UP * move.x * turn_sensitivity * -1.0
+		#apply_torque_impulse(impulse)
+		var pcam_rotation_degrees: Vector3
+		pcam_rotation_degrees = pcam.get_third_person_rotation_degrees()
+		pcam_rotation_degrees.x += input.y * -1.0
+		pcam_rotation_degrees.x = clampf(pcam_rotation_degrees.x, -80.0, 85.0)
+		pcam_rotation_degrees.y += input.x * -1.0
+		pcam_rotation_degrees.y = wrapf(pcam_rotation_degrees.y, 0.0, 360.0)
+		pcam.set_third_person_rotation_degrees(pcam_rotation_degrees)
 
 @export var jump_impulse : float = 10.0
 @export var rotation_input_force : float = 25.0
-@export var turn_sensitivity : float = 0.05
+@export var mouse_camera_sensitivity : float = 0.05
+@export var gamepad_camera_sensitivity : float = 1.0
 @export var turn_force : float = 100.0
 
 func _physics_process(delta: float) -> void:
 	var input_dir := JamUtils.get_move_input_dir_2d()
-	var turn_input := Input.get_action_strength("turn_left") - Input.get_action_strength("turn_right")
+	var turn_input := Input.get_axis("turn_right", "turn_left")
 	if input_dir.length_squared() > 0.01 or turn_input != 0.0:
 		var input_torque := Vector3(input_dir.y, 0.0, -input_dir.x) * rotation_input_force + Vector3.UP * turn_force * turn_input
 		input_torque = global_basis * input_torque
